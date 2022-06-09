c909d7344f3bb2944ddca02e524c0cbc7c857db1
DoDinhLoc <dinhloc0710@gmail.com>
2022-06-09 16:50:55 +0700

    hash salt password and create jwt for the login

---
+++ jwtWebApi/Controllers/AuthController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace jwtWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // create method register user
+
+
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+         {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password,out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.UserName = request.UserName;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            if(user.UserName != request.UserName)
+            {
+                return BadRequest("Người dùng không tồn tại");
+
+            }
+
+            if(!VerifyPasswordHash(request.Password, user.PasswordHash,user.PasswordSalt))
+            {
+                return BadRequest("Sai mật khẩu");
+            }
+
+            // tạo mã thông báo token 
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+               new Claim(ClaimTypes.Name, user.UserName),
+            };
+            // khóa bảo mật
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            // mã thông báo bảo mật mói 
+
+            var token = new JwtSecurityToken(
+                claims: claims, // xac nhận quyền
+                expires: DateTime.Now.AddDays(1), // ngày hết hạn , giá trị 1 ngày 
+                signingCredentials:creds
+                );
+
+            // chuỗi để mã thông báo 
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+
+            return jwt;
+        }
+
+
+
+
+        // method Hash password 
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+
+        // xác minh mật khẩu
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash); // so sánh từng byte giá trị ng dùng nhâp vào vs mk đã băm
+            }    
+        }
+
+    }
+}
---
+++ jwtWebApi/User.cs
@@ -0,0 +1,9 @@
+﻿namespace jwtWebApi
+{
+    public class User
+    {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } // tạo  băm mk 
+        public byte[] PasswordSalt { get; set; } // sắp xếp mk 
+    }
+}
